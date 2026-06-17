790a89c7ba77fc5db0a59b5c66828569ff3bf544
eitnadim <nadim@ethicalintelligent.com>
2026-06-18 00:54:18 +0530

    Add springboot/src/main/java/com/framework/v25/controller/AuthController.java

---
+++ springboot/src/main/java/com/framework/v25/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.framework.v25.controller;
+
+import com.framework.v25.dto.AuthResponse;
+import com.framework.v25.dto.LoginRequest;
+import com.framework.v25.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
