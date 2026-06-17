14460bdbc5e4117de7c62ba24240619946771a38
eitnadim <nadim@ethicalintelligent.com>
2026-06-18 00:54:31 +0530

    Add springboot/src/main/java/com/framework/v25/dto/DeployResult.java

---
+++ springboot/src/main/java/com/framework/v25/dto/DeployResult.java
@@ -0,0 +1,22 @@
+package com.framework.v25.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.extern.jackson.Jacksonized;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@Jacksonized
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeployResult {
+
+    private String         status;
+    private String         url;
+    private String         logs;
+    private OffsetDateTime timestamp;
+}
